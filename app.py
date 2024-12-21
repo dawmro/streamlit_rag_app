b36b1b96b912cc58b22e11e4bff5adbb9d475604
dawid mrotek <DawidMrotek@protonmail.ch>
2024-12-21 18:11:03 +0100

    [ADD] create vector db

--- app.py
+++ app.py
@@ -7,9 +7,15 @@ from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from streamlit.runtime.uploaded_file_manager import UploadedFile
 
+import chromadb
+from chromadb.utils.embedding_functions.ollama_embedding_function import (
+    OllamaEmbeddingFunction,
+)
+
 
 
 def process_document(uploaded_file: UploadedFile) -> list[Document]:
+
     temp_file = tempfile.NamedTemporaryFile("wb", suffix=".pdf", delete=False)
     temp_file.write(uploaded_file.read())
 
@@ -28,7 +34,46 @@ def process_document(uploaded_file: UploadedFile) -> list[Document]:
 
 
 
+def get_vector_collection() -> chromadb.Collection:
+    
+    # use ollama embedding function
+    ollama_ef = OllamaEmbeddingFunction(
+        url="http://localhost:11434/api/embeddings",
+        model_name="nomic-embed-text:latest",
+    )
+
+    # persist data on disk
+    chroma_client = chromadb.PersistentClient(path="./rag_chroma")
+    return chroma_client.get_or_create_collection(
+        name="rag_app",
+        embedding_function=ollama_ef,
+        metadata={"hnsw:space": "cosine"},
+    )
+
+
+
+def add_to_vector_collection(all_splits: list[Document], file_name: str):
+   
+    collection = get_vector_collection()
+    documents, metadatas, ids = [], [], []
+
+    for idx, split in enumerate(all_splits):
+        documents.append(split.page_content)
+        metadatas.append(split.metadata)
+        ids.append(f"{file_name}_{idx}")
+
+    # create or update data
+    collection.upsert(
+        documents=documents,
+        metadatas=metadatas,
+        ids=ids,
+    )
+    st.success("Data added to the vector store!")
+
+
+
 if __name__ == "__main__":
+
     # sidebar for document upload
     with st.sidebar:
         st.set_page_config(page_title="RAG App")
@@ -44,9 +89,12 @@ if __name__ == "__main__":
             "Chunk and store in vector db"
         )
 
-    if uploaded_file and process:
-        all_splits = process_document(uploaded_file=uploaded_file)
-        st.write(all_splits)
+        if uploaded_file and process:
+            normalize_uploaded_file_name = uploaded_file.name.translate(
+                str.maketrans({"-": "_", ".": "_", " ": "_"})
+            )
+            all_splits = process_document(uploaded_file)
+            add_to_vector_collection(all_splits, normalize_uploaded_file_name)
 
 
     
